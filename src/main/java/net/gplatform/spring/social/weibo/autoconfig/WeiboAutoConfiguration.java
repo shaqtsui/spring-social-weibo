90ba681e3ab5fedde5c06b81a6e52cbf5f16d565
xufucheng <xfcjscn@gmail.com>
2014-09-01 13:13:54 +0800

    move common to base

--- src/main/java/net/gplatform/spring/social/weibo/autoconfig/WeiboAutoConfiguration.java
+++ src/main/java/net/gplatform/spring/social/weibo/autoconfig/WeiboAutoConfiguration.java
@@ -1,16 +1,10 @@
 package net.gplatform.spring.social.weibo.autoconfig;
 
-import javax.sql.DataSource;
-
 import net.gplatform.spring.social.weibo.api.Weibo;
-import net.gplatform.spring.social.weibo.connect.JdbcUsersConnectionRepositoryTableCreator;
-import net.gplatform.spring.social.weibo.connect.SimpleConnectionSignUp;
 import net.gplatform.spring.social.weibo.connect.WeiboConnectionFactory;
 
-import org.apache.commons.beanutils.BeanUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -26,18 +20,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.core.env.Environment;
-import org.springframework.security.crypto.encrypt.Encryptors;
-import org.springframework.security.crypto.encrypt.TextEncryptor;
 import org.springframework.social.config.annotation.ConnectionFactoryConfigurer;
 import org.springframework.social.config.annotation.EnableSocial;
 import org.springframework.social.config.annotation.SocialConfigurerAdapter;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.ConnectionFactory;
-import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.ConnectionRepository;
-import org.springframework.social.connect.ConnectionSignUp;
-import org.springframework.social.connect.UsersConnectionRepository;
-import org.springframework.social.connect.jdbc.JdbcUsersConnectionRepository;
 import org.springframework.social.connect.web.GenericConnectionStatusView;
 import org.springframework.web.servlet.View;
 
@@ -56,36 +44,10 @@ public class WeiboAutoConfiguration {
 		private static final Logger LOG = LoggerFactory.getLogger(WeiboAutoConfigurationAdapter.class);
 		private RelaxedPropertyResolver properties;
 
-		@Autowired(required = false)
-		DataSource dataSource;
-
-		@Autowired
-		JdbcUsersConnectionRepositoryTableCreator jdbcUsersConnectionRepositoryTableCreator;
-
-		@Autowired
-		ConnectionSignUp connectionSignUp;
 
 		@Override
 		public void addConnectionFactories(ConnectionFactoryConfigurer configurer, Environment environment) {
 			configurer.addConnectionFactory(createConnectionFactory(this.properties));
-		}
-
-		@Override
-		public UsersConnectionRepository getUsersConnectionRepository(ConnectionFactoryLocator connectionFactoryLocator) {
-			UsersConnectionRepository usersConnectionRepository = null;
-			if (dataSource == null) {
-				usersConnectionRepository = super.getUsersConnectionRepository(connectionFactoryLocator);
-			} else {
-				jdbcUsersConnectionRepositoryTableCreator.createTableIfNotExist();
-				TextEncryptor textEncryptor = Encryptors.noOpText();
-				usersConnectionRepository = new JdbcUsersConnectionRepository(dataSource, connectionFactoryLocator, textEncryptor);
-			}
-			try {
-				BeanUtils.setProperty(usersConnectionRepository, "connectionSignUp", connectionSignUp);
-			} catch (Exception e) {
-				LOG.error("Error config ConnectionSignUp", e);
-			}
-			return usersConnectionRepository;
 		}
 
 		@Override
@@ -126,17 +88,5 @@ public class WeiboAutoConfiguration {
 		public View weiboConnectView() {
 			return new GenericConnectionStatusView("weibo", "Weibo");
 		}
-
-		@Bean
-		public JdbcUsersConnectionRepositoryTableCreator jdbcUsersConnectionRepositoryTableCreator() {
-			return new JdbcUsersConnectionRepositoryTableCreator();
-		}
-
-		@Bean
-		@ConditionalOnMissingBean(ConnectionSignUp.class)
-		public ConnectionSignUp connectionSignUp() {
-			return new SimpleConnectionSignUp();
-		}
-
 	}
 }
