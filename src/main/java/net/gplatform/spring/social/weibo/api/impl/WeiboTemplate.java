caa5a5d9c6269a3c8bd870c994ebee64b5fb1d5b
xufucheng <xfcjscn@gmail.com>
2014-09-02 12:15:30 +0800

    add license

--- src/main/java/net/gplatform/spring/social/weibo/api/impl/WeiboTemplate.java
+++ src/main/java/net/gplatform/spring/social/weibo/api/impl/WeiboTemplate.java
@@ -15,6 +15,28 @@
  */
 package net.gplatform.spring.social.weibo.api.impl;
 
+/*
+ * #%L
+ * spring-social-weibo
+ * %%
+ * Copyright (C) 2013 - 2014 Shark Xu
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
 import net.gplatform.spring.social.weibo.api.Weibo;
 import weibo4j.Account;
 import weibo4j.Comments;
